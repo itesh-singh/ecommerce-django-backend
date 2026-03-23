673d13b9c33f98b11f4be0cee68688e9a465b672
itesh-singh <itesh5906@gmail.com>
2026-03-23 22:00:59 +0530

    fix(home): remove invalid review loop and prevent unbound variable error

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -3,11 +3,8 @@ from store.models import Product, ReviewRating
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')
-
-    # Get the reviews
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+    reviews = ReviewRating.objects.filter(status=True)
 
     context = {
         'products': products,
